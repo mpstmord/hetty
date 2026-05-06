3e59fad8a6d031532d462ba130595bd11a82aa6a
Morgan Stmord <mpstmord@users.noreply.github.com>
2026-05-07 06:05:41 +0800

    Add reminder comment about opening admin UI in browser after startup

--- cmd/hetty/main.go
+++ cmd/hetty/main.go
@@ -68,6 +68,9 @@ func main() {
 	log.Printf("[INFO] Listening on %v", *addr)
 	log.Printf("[INFO] Admin interface available at http://%v%v", *addr, *adminPath)
 
+	// Reminder: open the admin UI in the browser after startup for easier access.
+	// e.g. http://127.0.0.1:8080/hetty/
+
 	// TODO: Initialize proxy, database, and HTTP server.
 	// This will be wired up as the project progresses.
 	_ = adminPath
