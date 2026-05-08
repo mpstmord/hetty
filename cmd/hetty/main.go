018f84d021b2270353de5397ba9b7acea0f3f12b
Morgan Stmord <mpstmord@users.noreply.github.com>
2026-05-08 09:53:28 +0800

    Add reminder comment about importing CA cert for HTTPS interception

--- cmd/hetty/main.go
+++ cmd/hetty/main.go
@@ -81,9 +81,10 @@ func main() {
 	// Personal note: open the admin UI in the browser after startup.
 	// Shortcut: http://127.0.0.1:8080/hetty/
 	// Tip: use -upstreamProxy to chain through Burp Suite for extra analysis.
+	//
+	// Reminder: make sure the hetty CA cert is imported into the browser/OS trust
+	// store before intercepting HTTPS traffic, otherwise you'll get TLS errors.
+	// The CA cert lives in the certs directory printed above.
 
 	// TODO: Initialize proxy, database, and HTTP server.
-	// This will be wired up as the project progresses.
-	_ = adminPath
-	_ = verbose
-}
+	// This will be wired up as the project prog
